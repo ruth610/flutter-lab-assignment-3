2477b4cd5457b10869de68ce39927e0b8911899e
ruth610 <106428054+ruth610@users.noreply.github.com>
2025-05-24 16:52:35 +0300

    updated to httpClient from retrofit

--- flutter_lab_assignment_3/lib/data/repository/album_repository.dart
+++ flutter_lab_assignment_3/lib/data/repository/album_repository.dart
@@ -1,18 +1,38 @@
-import 'package:dio/dio.dart';
-import '../api/retrofit_client.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import '../models/album.dart';
 import '../models/photo.dart';
 
 class AlbumRepository {
-  final Dio dio = RetrofitClient().dio;
+  final HttpClient _httpClient = HttpClient();
+  final String _baseUrl = "https://jsonplaceholder.typicode.com/";
 
   Future<List<Album>> fetchAlbums() async {
-    final response = await dio.get("albums");
-    return (response.data as List).map((json) => Album.fromJson(json)).toList();
+    final url = Uri.parse("${_baseUrl}albums");
+    final request = await _httpClient.getUrl(url);
+    final response = await request.close();
+
+    if (response.statusCode == 200) {
+      final responseBody = await response.transform(utf8.decoder).join();
+      final List<dynamic> jsonList = json.decode(responseBody);
+      return jsonList.map((json) => Album.fromJson(json)).toList();
+    } else {
+      throw Exception("Failed to load albums");
+    }
   }
 
   Future<List<Photo>> fetchPhotos(int albumId) async {
-    final response = await dio.get("photos", queryParameters: {"albumId": albumId});
-    return (response.data as List).map((json) => Photo.fromJson(json)).toList();
+    final url = Uri.parse("${_baseUrl}photos?albumId=$albumId");
+    final request = await _httpClient.getUrl(url);
+    final response = await request.close();
+
+    if (response.statusCode == 200) {
+      final responseBody = await response.transform(utf8.decoder).join();
+      final List<dynamic> jsonList = json.decode(responseBody);
+      return jsonList.map((json) => Photo.fromJson(json)).toList();
+    } else {
+      throw Exception("Failed to load photos");
+    }
   }
 }
--- flutter_lab_assignment_3/lib/data/repository/photo_repository.dart
+++ flutter_lab_assignment_3/lib/data/repository/photo_repository.dart
@@ -1,20 +1,23 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import '../models/photo.dart';
 
 class PhotoRepository {
-  final Dio _dio = Dio();
+  final HttpClient _httpClient = HttpClient();
 
   Future<List<Photo>> fetchPhotos(int albumId) async {
     try {
-      final response = await _dio.get(
-        'https://jsonplaceholder.typicode.com/albums/$albumId/photos',
-      );
+      final url = Uri.parse('https://jsonplaceholder.typicode.com/albums/$albumId/photos');
+      final request = await _httpClient.getUrl(url);
+      final response = await request.close();
 
       if (response.statusCode == 200) {
-        final List<dynamic> jsonList = response.data;
+        final responseBody = await response.transform(utf8.decoder).join();
+        final List<dynamic> jsonList = json.decode(responseBody);
         return jsonList.map((json) => Photo.fromJson(json)).toList();
       } else {
-        throw Exception('Failed to load photos');
+        throw Exception('Failed to load photos: ${response.statusCode}');
       }
     } catch (e) {
       throw Exception('PhotoRepository Error: $e');
